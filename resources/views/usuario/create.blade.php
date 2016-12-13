53d028d9ba34826ae0dc001a17aca0474600f09b
deivismartinez <deivismartinez77@gmail.com>
2016-12-13 16:42:56 -0500

    first commit

---
+++ resources/views/usuario/create.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.admin');
+
+@section('content')
+<form action="">
+    <div class="form-group">
+        <label for="">Nombre</label>
+        <input type="text" class="form-control">
+    </div>
+</form>
+@stop
